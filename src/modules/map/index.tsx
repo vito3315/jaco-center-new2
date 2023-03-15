35e0b16345ec176df59711fd6bf9c4b69a7acc16
Denis <adadenis1982@yandex.ru>
2023-03-15 15:59:13 +0300

    remove map

--- src/modules/map/index.tsx
+++
@@ -1,7 +0,0 @@
-import { useMap } from './store';
-import { mapState } from './types';
-import Form from "./form";
-import Map from "./map"
-
-export { useMap, Form, Map };  
-export type { mapState };
